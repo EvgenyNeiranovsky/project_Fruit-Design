a1ec4fb4f49a11434bbb5afe5b13190d4729177f
Evgeny Neiranovsky <e.neiranovsky@gmail.com>
2022-03-30 02:35:05 +0300

    03/30/2022 am 2:35

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log('fghjkl');
+const menu = document.getElementById('menu');
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+
+window.ontouchstart = function(event) {
+    console.log('123')
+    if(event.target == close) {
+        menu.classList.add('header__menu--close');
+    } 
+
+    if(event.target == open ) {
+        menu.classList.remove ('header__menu--close');
+    } 
+    
+        
+    }
